a56c1a11d52ff33782d142c9d5ad734c2eeac029
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-09-20 20:56:13 +0900

    Change class name & Remove unnecessary code
    Change class name & Remove unnecessary code

--- Profiler/InstrumentationTimer.hpp
+++ Profiler/InstrumentationTimer.hpp
@@ -1,26 +1,26 @@
 #pragma once
 #include "Profiler.hpp"
-#include <cassert>
-// TraceGenerator
+#include <chrono>
+
+// InstrumentationTimer
 // RAII style
 namespace prof {
-	class TraceGenerator {
+	class InstrumentationTimer {
 	private:
 		std::chrono::steady_clock::time_point m_start;
-		std::string m_name;
+		std::string_view m_name;
 		static const long long m_initTime;
 	public:
-		TraceGenerator(const std::string& name)
+		InstrumentationTimer(std::string_view name)
 			: m_start(std::chrono::high_resolution_clock::now()), m_name(name) {}
-		~TraceGenerator() {
+		~InstrumentationTimer() {
 			// Get times in microseconds
 			auto startTime = std::chrono::duration_cast<std::chrono::microseconds>(m_start.time_since_epoch()).count();
 			auto endTime = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count();
 			// Get Thread id
 			auto threadID = std::hash<std::thread::id>()(std::this_thread::get_id());
-			assert(startTime > m_initTime);
-			// Send Trace
-			Profiler::sendTrace(m_name, endTime - startTime, startTime - m_initTime, threadID);
+			// Process Trace
+			Profiler::processTrace(m_name, endTime - startTime, startTime - m_initTime, threadID);
 		}
 	};
 }
@@ -31,7 +31,7 @@ namespace prof {
 // Set it to 0 otherwise
 #define PROFILE 1
 #if PROFILE
-#define PROFILE_SCOPE(name) prof::TraceGenerator tracer##__LINE__(name)
+#define PROFILE_SCOPE(name) prof::InstrumentationTimer tracer##__LINE__(name)
 #else
 #define PROFILE_SCOPE(name)
 #endif
--- Profiler/TraceGenerator.cpp
+++
@@ -1,2 +0,0 @@
-#include "TraceGenerator.hpp"
-const long long prof::TraceGenerator::m_initTime{ std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now().time_since_epoch()).count() };
