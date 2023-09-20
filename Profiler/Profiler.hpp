8b96e60c6b37ad412909ad2efc339dd2bb1a0253
Sangin Geum <56298463+sangingeum@users.noreply.github.com>
2023-09-20 20:56:46 +0900

    Use string_view & Remove unnecessary code
    Use string_view & Remove unnecessary code

--- Profiler/Profiler.hpp
+++ Profiler/Profiler.hpp
@@ -1,12 +1,8 @@
 #pragma once
-#include <string>
 #include <fstream>
-#include <chrono>
-#include <thread>
-#include <string_view>
-#include <queue>
-#include <atomic>
 #include <format>
+#include <string_view>
+
 namespace prof {
 	// Singleton Profiler
 	// RAII style
@@ -14,10 +10,9 @@ namespace prof {
 	{
 	private:
 		std::ofstream m_outFile;
-		std::queue<std::tuple<>> m_traceQueue;
 	public:
 		// Times are given in microseconds
-		static void sendTrace(const std::string& name, long long duration, long long startTime, size_t threadID) {
+		static void processTrace(std::string_view name, long long duration, long long startTime, size_t threadID) {
 			static Profiler profiler;
 			profiler.writeTrace(name, duration, startTime, threadID);
 		}
@@ -25,18 +20,17 @@ namespace prof {
 		Profiler(const std::string& fileName = "profile_result.json") {
 			m_outFile.open(fileName);
 			m_outFile << "{\"traceEvents\":[";
-			std::thread traceProcessingThread();
 		}
 		Profiler(const Profiler&) = delete;
 		Profiler& operator=(const Profiler&) = delete;
 		~Profiler() {
-			long pos = m_outFile.tellp();
+			size_t pos = m_outFile.tellp();
 			// Erase the last comma
 			m_outFile.seekp(pos - 1);
 			m_outFile << "]}";
 			m_outFile.close();
 		}
-		void writeTrace(const std::string& name, long long duration, long long startTime, size_t threadID) {
+		void writeTrace(std::string_view name, long long duration, long long startTime, size_t threadID) {
 			m_outFile <<  "{" + std::format("\"name\":\"{}\",\"dur\":{},\"ph\":\"X\",\"pid\":0,\"tid\":{},\"ts\":{}", name, duration, threadID, startTime) + "},";
 		}
 	};
--- Profiler/main.cpp
+++ Profiler/main.cpp
@@ -1,23 +1,19 @@
-#include "TraceGenerator.hpp"
+#include "InstrumentationTimer.hpp"
 #include <iostream>
 
 void function1() {
 	PROFILE_FUNCTION();
 	for (size_t i = 0; i < 1000; ++i) {
-		PROFILE_SCOPE("function1 loop");
 		std::cout << "function1\n";
 	}
-
 }
 
 void function2() {
 	PROFILE_FUNCTION();
 	for (size_t i = 0; i < 1000; ++i) {
-		PROFILE_SCOPE("function2 loop");
 		std::cout << "function2\n";
 	}
 }
-
 
 int main() {
 	PROFILE_FUNCTION();
